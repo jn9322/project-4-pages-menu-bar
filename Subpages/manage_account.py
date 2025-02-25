65ffd85b654e3f43f8a6e16d3dbc92c3462b04af
jn9322 <honza.ne@seznam.cz>
2025-02-25 16:06:06 +0000

    v_25-Feb-2025

--- Subpages/manage_account.py
+++ Subpages/manage_account.py
@@ -1,3 +1,6 @@
 import streamlit as st
 
 st.write("Ahoj - manage account page")
+
+if st.button("Klikni zde :)"):
+    st.info("Kliknuls blbecku")
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ pages = {
     "Obsah": [
         st.Page("Subpages/create_account.py", title="Create your account"),
         st.Page("Subpages/manage_account.py", title="Manage your account"),
+        st.Page("Subpages/Pozor tlacitka - sequnce.py", title="POZOR tlacitka sequence"),
     ]
 }
 
